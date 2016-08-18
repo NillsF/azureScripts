2061520348f6b25bc853e0723ff86f5753f3f299
NillsF <nills.franssens@gmail.com>
2016-08-18 16:11:52 +0200

    VNET peering setup

---
+++ PeeringSetup.ps1
@@ -0,0 +1,19 @@
+﻿#logging into azure
+add-azurermaccount
+#register the feature
+Register-AzureRmProviderFeature -FeatureName AllowVnetPeering -ProviderNamespace Microsoft.Network
+
+Register-AzureRmResourceProvider -ProviderNamespace Microsoft.Network
+#My variables, sorry for the bad naming; I did this a bit too quickly
+$vnet1name = "VNETpeering-vnet"
+$vnet2name = "VNETpeeringvnet949"
+$rgname = "VNETpeering"
+
+#get vnets
+$vnet1 = Get-AzureRmVirtualNetwork -ResourceGroupName $rgname -Name $vnet1name
+$vnet2 = Get-AzureRmVirtualNetwork -ResourceGroupName $rgname -Name $vnet2name
+
+#now this is a bit tricky, a peering relationship needs to be setup twice, once 1==>2; and once 2==>1
+Add-AzureRmVirtualNetworkPeering -name From1To2 -VirtualNetwork $vnet1 -RemoteVirtualNetworkId $vnet2.id 
+Add-AzureRmVirtualNetworkPeering -name From2To1 -VirtualNetwork $vnet2 -RemoteVirtualNetworkId $vnet1.id 
+#all setup and ready to go
