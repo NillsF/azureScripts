1a79e6e098c8e54f0a03de4f4dff7fed6d53cf91
Nills Franssens <nills.franssens@gmail.com>
2019-06-16 18:59:02 -0700

    Create aksworkshop-setup.sh

---
+++ aksworkshop-setup.sh
@@ -0,0 +1,26 @@
+###
+#This file contains a shell script to setup Ubuntu to install docker, kubectl and the azure cli
+###
+
+sudo apt-get update
+#install docker
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+#install kubectl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+#install az-cli
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
